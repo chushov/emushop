f9e17bd32ee7e13473eec56417ae7c53fc1dae9d
chushov <work@chushov.com>
2021-08-13 06:52:33 +0300

    Разбирался со статикой, добавил конфиг

--- application/settings.py
+++ application/settings.py
@@ -120,10 +120,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATICFILES_DIRS = (
     (BASE_DIR / 'static_dev'),
 )
+
+# STATICFILES_DIRS = os.path.join(BASE_DIR, "static_dev")
