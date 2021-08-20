962ff46041a6bd6e205c5e26b37e28310283b804
chushov <work@chushov.com>
2021-08-20 14:01:47 +0300

    Поправил работу с урлами

--- musicshop/views.py
+++ musicshop/views.py
@@ -2,11 +2,7 @@ from django.shortcuts import render
 from django import views
 
 
-class BaseView():
+class BaseView(views.View):
 
     def get(self, request, *args, **kwargs):
         return render(request, 'base.html', )
-
-    @classmethod
-    def as_view(cls):
-        pass
