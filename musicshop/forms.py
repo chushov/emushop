035b1730aaf46f0f154953add8ae189118c9728f
chushov <work@chushov.com>
2021-08-23 20:03:20 +0300

    Вьюхи для регистрации пользователей на сайте

--- musicshop/forms.py
+++ musicshop/forms.py
@@ -28,5 +28,47 @@ class LoginForm(forms.ModelForm):
         return self.cleaned_data
 
 
-class RegistrationForm():
+class RegistrationForm(forms.ModelForm):
 
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+    phone = forms.CharField(required=False)
+    address = forms.CharField(required=False)
+    email = forms.EmailField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'Логин'
+        self.fields['password'].label = 'Пароль'
+        self.fields['confirm_password'].label = 'Подтвердите пароль'
+        self.fields['phone'].label = 'Телефон'
+        self.fields['address'].label = 'Адрес'
+        self.fields['email'].label = 'Емэйл'
+        self.fields['first_name'].label = 'Имя'
+        self.fields['last_name'].label = 'Фамилия'
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        domain = email.split('.')[-1]
+        if domain in ['net', 'xyz']:
+            raise forms.ValidationError(f'Регистрация для домена {domain} невозможна.')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError(f'Такой ящик уже зарегистрирован')
+        return email
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError(f'К сожалению, имя {username} уже занято. Попробуйте придумать другое.')
+        return username
+
+    def clean(self):
+        password = self.cleaned_data['password']
+        confirm_password = self.cleaned_data['confirm_password']
+        if password != confirm_password:
+            raise forms.ValidationError('Пароли не совпадают')
+        return self.cleaned_data
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'confirm_password', 'first_name', 'last_name', 'address', 'phone', 'email']
